c67cab49874fda8edea6d85fa3edb1fe9a85d816
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-03-30 11:45:43 +0530

    added work file converter

--- src/collection/items/pecha.py
+++ src/collection/items/pecha.py
@@ -1,14 +1,17 @@
 from collection.items.item import Item
 from dataclasses import dataclass
+from typing import Dict
 
 @dataclass(frozen=True)
 class Pecha:
     id: str
     title: str
-    bdrc_id: str
-    volume_number:str
-    base_name:str
-    pecha_path:str
+    bdrc_work_id: str
+    path:str
 
 if __name__ == "__main__":
     print(Pecha.__annotations__)
+
+class PechaFragment(Pecha):
+    "span= {'base':(start,end)}"
+    span: Dict
